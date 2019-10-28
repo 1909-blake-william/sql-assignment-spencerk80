d86a4e65631812bbfd34162804663af5aca1666a
Kristoffer Spencer <whobanil80@linuxmail.org>
2019-10-28 11:43:53 -0400

    Made it through 2.x

--- Revature Associate SQL Workbook.sql
+++ Revature Associate SQL Workbook.sql
@@ -1,29 +1,73 @@
 --In this section you will begin the process of working with the Oracle Chinook database
 --1.0 Setting up Oracle Chinook
 --Task � Open the Chinook_Oracle.sql file and execute the scripts within.
+--
+-- Okay, done 
+--
 --2.0 SQL Queries
 --In this section you will be performing various queries against the Oracle Chinook database.
 --2.1 SELECT
 --Task � Select all records from the Employee table.
+SELECT * FROM employee;
 --Task � Select all records from the Employee table where last name is King.
+SELECT * FROM employee WHERE lastname = 'King';
 --Task � Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
+SELECT * FROM employee WHERE firstname = 'Andrew' AND reportsto IS NULL;
 --2.2 ORDER BY
 --Task � Select all albums in Album table and sort result set in descending order by title.
+SELECT * FROM album ORDER BY title DESC;
 --Task � Select first name from Customer and sort result set in ascending order by city
+SELECT firstname FROM customer ORDER BY firstname ASC;
 --2.3 INSERT INTO
 --Task � Insert two new records into Genre table
+INSERT INTO genre (genreid, name) VALUES (101, 'Metalcore');
+INSERT INTO genre (genreid, name) VALUES (102, 'Folk Metal');
 --Task � Insert two new records into Employee table
+INSERT INTO employee (employeeid, lastname, firstname, title, reportsto, birthdate, hiredate, address, city, state, country, postalcode, phone, fax, email)
+VALUES (9, 'Spencer', 'Kris', 'IT Staff', 6, '06-JUN-91', '05-MAY-06', '1221 Fake ST', 'Edmonton', 'AB', 'Canada', 'T5K 2N1', '+1 (780) 555-3344', '+1 (234) 235-2362', 'spenc_k545@chinookcorp.com');
+INSERT INTO employee (employeeid, lastname, firstname, title, reportsto, birthdate, hiredate, address, city, state, country, postalcode, phone, fax, email)
+VALUES (10, 'Johnson', 'Patrick', 'Sales Support Agent', 2, '05-MAY-92', '05-MAY-06', '1221 Fake ST', 'Edmonton', 'AB', 'Canada', 'T5k 2N1',  '+1 (780) 555-3344', '+1 (234) 235-2362', 'John_p488@chinookcorpg.com');
 --Task � Insert two new records into Customer table
+INSERT INTO customer (customerid, firstname, lastname, company, address, city, state, country, postalcode, phone, fax, email, supportrepid)
+VALUES (60, 'Bob', 'Dunn', null, '234 Some ST', 'Redmon', 'WA', 'USA', '98052-8300', '+1 (425) 555-4878', null, 'Dude@blah.com', 3);
+INSERT INTO customer (customerid, firstname, lastname, company, address, city, state, country, postalcode, phone, fax, email, supportrepid)
+VALUES (61, 'Elain', 'Dunn', null, '234 Some ST', 'Redmon', 'WA', 'USA', '98052-8300', '+1 (425) 555-4878', null, 'Lady@blah.com', 3);
 --2.4 UPDATE
 --Task � Update Aaron Mitchell in Customer table to Robert Walter
+UPDATE customer
+SET firstname = 'Rober', lastname = 'Walter'
+WHERE firstname = 'Aaron' AND lastname = 'Mitchell';
 --Task � Update name of artist in the Artist table �Creedence Clearwater Revival� to �CCR�
+UPDATE artist
+SET name = 'CCR'
+WHERE name = 'Creedence Clearwater Revival';
 --2.5 LIKE
 --Task � Select all invoices with a billing address like �T%�
+SELECT * FROM invoice
+WHERE billingaddress LIKE 'T%';
 --2.6 BETWEEN
 --Task � Select all invoices that have a total between 15 and 50
+SELECT * FROM invoice
+WHERE total BETWEEN 15 AND 50;
 --Task � Select all employees hired between 1st of June 2003 and 1st of March 2004
+SELECT * FROM employee
+WHERE hiredate BETWEEN '01-JUN-03' AND '01-MAR-04';
 --2.7 DELETE
---Task � Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
+--Task � Delete a recordin Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
+--NOTE: the name was in there wrong...
+UPDATE customer
+SET firstname = 'Robert'
+WHERE firstname = 'Rober';
+
+DELETE FROM invoiceline
+WHERE invoiceid IN (SELECT invoiceid FROM invoice
+                    WHERE customerid = (SELECT customerid FROM customer
+                                        WHERE firstname = 'Robert' AND lastname = 'Walter'));
+DELETE FROM invoice
+WHERE customerid = (SELECT customerid FROM customer
+                    WHERE firstname = 'Robert' AND lastname = 'Walter');
+DELETE FROM customer
+WHERE firstname = 'Robert' AND lastname = 'Walter';
 
 --3.0 SQL Functions
 --In this section you will be using the Oracle system functions, as well as your own functions, to perform various actions against the database
